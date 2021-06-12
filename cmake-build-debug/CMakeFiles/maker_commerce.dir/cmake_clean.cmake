1964ca028ef1b98c02b0f2a0c56cad3b5f2c1bcc
jaikme <jaime.mqu@gmail.com>
2021-06-11 23:38:10 -0300

    add: beginner files

---
+++ cmake-build-debug/CMakeFiles/maker_commerce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maker_commerce.dir/main.c.o"
+  "maker_commerce"
+  "maker_commerce.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/maker_commerce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
