267a807c38fd3a6e6c95bdef905df8d7d28b607a
jaikme <jaime.mqu@gmail.com>
2021-06-21 21:44:21 -0300

    add: regiter product

--- main.c
+++ main.c
@@ -10,18 +10,35 @@ int getMenuOption();
 
 // ──────────────────────────────────────
 // Definições globais
-#define MAX_STR_LENGTH 64
 #define HEADER_NAME "SUNLIGHT YELLOW"
+#define MAX_STR_LENGTH 64
+#define BOX_WIDTH 60
+#define MAX_PRODUCTS 1
 
 // ──────────────────────────────────────
 // Tipos customizados do programa
-typedef char String[MAX_STR_LENGTH];
+typedef char string[MAX_STR_LENGTH];
 
 // ──────────────────────────────────────
 // Constantes do programa
-#define MAX_STR_LENGTH 64
-#define HEADER_NAME "SUNLIGHT YELLOW"
+const int IDX_REGISTER_PRODUCT = 1;
+const int IDX_REMOVE_PRODUCT = 2;
+const int IDX_REPORT_PRODUCT = 3;
 
+// ──────────────────────────────────────
+// Variáveis e estruturas
+struct User {
+  int balance;
+};
+
+struct Products {
+  char name[30];
+  int cod;
+  float amount;
+};
+
+struct Products products[MAX_PRODUCTS];
+int productsTotal = 0;
 
 /*
  typedef struct {
@@ -53,6 +70,24 @@ struct MenuItem {
 
 
 // ──────────────────────────────────────
+// Métodos de utilidades
+
+int calcLength(const string value) {
+  int length = 0;
+  // Calcula a quantidade e caracteres da string value
+  while (value[length] != '\0') { ++length; }
+  return length;
+}
+
+int calcPad( int width, int length) {
+  return  (width - length) / 2;
+}
+
+void printError(string value) {
+  printf(RED "\n⛔️ %s" reset, value);
+}
+
+// ──────────────────────────────────────
 // Métodos do Cabeçalho
 
 void printDoubleLine(int width) {
@@ -63,15 +98,22 @@ void printDoubleLine(int width) {
   }
 }
 
-void displayHeading(String value) {
+void printLine(int width, bool newLine) {
+  unsigned i = width;
+  if(newLine) printf("\n" HYELLOW);
+  while(i > 0) {
+    i--;
+    printf("─");
+  }
+  if(newLine) printf(reset "\n");
+}
+
+void displayHeading(string value) {
   /*
    * width  - largura interna da caixa(não contando as bordas)
    * length - quantidade e caracteres
    */
-  int width = 60, length = 0;
-
-  // Calcula a quantidade e caracteres da string value
-  while (value[length] != '\0') { ++length; }
+  int width = BOX_WIDTH, length = calcLength(value);
 
   // Desenha o início da caixa do cabeçalho com cor amarela
   printf("%s╔", HYELLOW);
@@ -81,7 +123,7 @@ void displayHeading(String value) {
   printf("╗\n║");
 
   // Desenha o miolo da caixa com o título
-  int paddingWidth = (width - length) / 2;
+  int paddingWidth = calcPad(width, length);
   int paddingOdd = length % 2 ? paddingWidth + 1 : paddingWidth;
 
   printf("%s%*s%s%*s%s", IYELLOW, paddingWidth, "", value, paddingOdd, "", reset);
@@ -94,46 +136,126 @@ void displayHeading(String value) {
   printf("╝\n%s", reset);
 }
 
+void displayChoiceHeading(string value) {
+  int width = BOX_WIDTH, length = calcLength(value);
+  int paddingWidth = calcPad(width, length);
+  printf("\n" HYELLOW);
+  printLine(paddingWidth, false);
+  printf(" %s ", value);
+  printLine(paddingWidth, false);
+  printf("\n\n" reset);
+}
 
 
 // ──────────────────────────────────────
 // Métodos do Menu
 
-void displayText(String value) {
+void displayText(string value) {
   printf("\n%s", value);
 }
 
-//void displayMenuItem(MenuOption opt) {
-//  printf("\n░ %d → %s", opt.index, opt.label);
-//}
-
-void displayMenuItem(int index, String label) {
-  printf("\n%s%d → %s%s", HBLU, index, label, reset);
+void displayMenuTitle(string value) {
+  printf("\n\n%s☞ %s%s", IYELLOW, value, reset);
 }
 
+void displayMenuItem(int index, string label) {
+  printf("\n%s%d) %s%s", HCYN, index, label, reset);
+}
+
+void renderMenu() {
+  /*
+  * ╭─────────────────────────╮
+  * │ OPÇÕES DE ADMINISTRAÇÃO │
+  * ╰─────────────────────────╯
+  */
+  displayMenuTitle("Opções de administração:");
+
+  displayMenuItem(IDX_REGISTER_PRODUCT, "Cadastrar produtos");
+  displayMenuItem(IDX_REMOVE_PRODUCT, "Remover produto");
+  displayMenuItem(IDX_REPORT_PRODUCT, "Relatório de produtos");
+
+
+
+  /*
+   * ╭───────────────────╮
+   * │ OPÇÕES DE COMPRAS │
+   * ╰───────────────────╯
+   */
+  displayMenuTitle("Opções de arrecadação:");
+
+  displayMenuItem(4, "Adicionar produtos");
+  displayMenuItem(5, "Remove produtos");
+  displayMenuItem(6, "Produtos selecionados");
+  displayMenuItem(7, "Pagar");
+}
+
+
+// ──────────────────────────────────────
+// Cadastro de produtos
+void printInput(string value) {
+  printf("%s%s: %s", value, HYELLOW, reset);
+}
+
+void registerProduct() {
+
+  if(productsTotal >= MAX_PRODUCTS) {
+    printError("Número máximo de produtos cadastrados!");
+    return;
+  }
+
+  printf("\n");
+  displayChoiceHeading("Cadastrar produto");
+
+
+  printInput("Codigo do produto");
+  scanf("%d", &products[productsTotal].cod);
+
+  printInput("Nome do produto");
+  // printf("  Nome do produto: \n  %s→%s ", HGRENN, reset);
+  scanf("%s", &products[productsTotal].name);
+
+  printInput("Valor do Produto");
+  scanf("%f", &products[productsTotal].amount);
+
+  puts("✅ Produto cadastrado\n");
+
+  productsTotal++;
+}
 
 int main() {
   // Aceitando Acentos
   setlocale (LC_ALL, "");
 
+  struct User user;
+  int menuCode;
+
+  // Quanto de dinheiro o usuário tem quando abre o app
+  // Esse valor pode ser uma consulta a um banco
+  user.balance = 1375;
+
+  // Começa entrada do menu e boas vindas
   displayHeading(HEADER_NAME);
 
-  displayText("BEM-VINDO, SELECIONE UMA OPÇÃO:\n");
+  // Mostra mensagem de boas vindas
+  displayText("BEM-VINDO, SELECIONE UMA OPÇÃO:");
 
-  displayMenuItem(1, "Cadastrar produtos");
-  displayMenuItem(2, "Remover produto");
+  // Renderiza menu e computa ações do usuário
+  do {
+    renderMenu();
+    menuCode = getMenuOption();
+    switch (menuCode) {
+      case 0: break;
 
-  displayMenuItem(2, "Relatório de produtos");
-  displayMenuItem(3, "Cadastrar produtos");
-  displayMenuItem(4, "Cadastrar produtos");
+      case IDX_REGISTER_PRODUCT:
+        registerProduct();
+        break;
 
-//  displayMenuItem(1, "Cadastrar produtos");
-//  displayMenuItem(2, "Listar produtos");
-//  displayMenuItem(2, "Listar produtos");
+      default:
+        printError("Opção inválida");
+    }
 
-  int menuCode = getMenuOption();
+  } while (menuCode != 0);
 
-  printf("%d", menuCode);
   return 0;
 }
 
@@ -141,7 +263,7 @@ int main() {
 int getMenuOption() {
   int n;
 
-  displayText("\nCodigo: ");
+  displayText(HMAG "\nCodigo: " reset);
   scanf("%d", &n);
 
   return n;
